cd6f097a3315338f40a39c5b2e4d75b42f3dbfc2
BuildTools <hararaengineer@gmail.com>
2021-04-08 00:06:09 +0900

    CRUD

---
+++ simple_board/app/Http/Controllers/MessagesController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Message;
+
+class MessagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //一覧習得
+        $messages = Message::orderBy('id', 'desc')->paginate(25);
+
+        return view('messages.index', [
+            'messages' => $messages,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $message = new Message;
+
+        return view('messages.create', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //バリデーション
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max:255',
+        ]);
+
+        //メッセージ作成
+        $message = new Message;
+        $message->title = $request->title;
+        $message->content = $request->content;
+        $message->save();
+        //リダイレクト
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $message = Message::findOrFail($id);
+
+        return view('messages.show', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $message = Message::findOrFail($id);
+
+        return view('messages.edit', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //バリデーション
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max:255'
+        ]);
+
+        //メッセージ取得
+        $message = Message::findOrFail($id);
+        //更新処理
+        $message->title = $request->title;
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $message = Message::findOrFail($id);
+
+        $message->delete();
+
+        return redirect('/');
+    }
+}
--- simple_board/routes/web.php
+++ simple_board/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
+
+//個別詳細ページ表示
+// Route::get('messages/{id}', 'MessagesController@show');
+//新規登録処理
+// Route::post('messages', 'MessagesController@store');
+//更新処理
+// Route::put('messages/{id}', 'MessagesController@update');
+//削除処理
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+//一覧ページindex(showの補助)
+// Route::get('messages', 'MessagesController@index'))->name('messages.index');
+//新規作成フォーム
+// Route::get('messages/create', 'MessagesController@create'))->name('messages.create');
+//更新フォーム
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
